9f7daa77a6a062cc1d74bbfdd2d4562f8be55f17
Chas Threlkeld <chathrel@indiana.edu>
2018-04-02 16:57:55 -0400

    Replaced timit corpus with the
    http://www.clips.uantwerpen.be/conll2000/chunking/ sets

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,15 @@
 object Main extends App {
+
+  // Pipeline Idea:
+  // Tokenize input
+  // -> POS tagging
+  // -> Break into sentences
+  // -> Tag stop-words or phrases ("I mean", "that is", "actually"...)
+  // -> greedy re-parse sentence
+
+  // Example:
+  // I would like a blue coat I mean a red coat
+  // I/PPN would/
+
   println("Hello World!")
 }
