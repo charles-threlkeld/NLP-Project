d177f68f0e50ee555c3860dc815a88b1c2fedca2
Chas Threlkeld <chathrel@indiana.edu>
2018-04-03 12:31:57 -0400

    Expanded outline in Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,7 +9,11 @@ object Main extends App {
 
   // Example:
   // I would like a blue coat I mean a red coat
-  // I/PPN would/
+  // I/Pronoun would/Verb like/Verb a/Determiner blue/Adjective coat/Noun I/Pronoun mean/Verb a/Determiner red/Adjective coat/Noun
+  //"I mean" -> stop word
+  // Tuple(List(Pronoun,Verb,Verb,Determiner,Adjective,Noun),List(Determiner,Adjective,Noun))
+  // List("I", "would", "like", "a", "red", "coat")
+  // String: "I would like a red coat"
 
   println("Hello World!")
 }
