924aac6e4d95d535fb2ea6d58dc3cd1ff2f7ebf3
Alexandru Ungureanu <alex@Alexandrus-MacBook-Pro.local>
2018-04-05 00:19:33 -0400

    skeletons for Sentencizer and Tokenizer

--- src/main/scala/Sentenceizer.scala
+++ src/main/scala/Sentenceizer.scala
@@ -1,5 +1,16 @@
 package nltk
 
 class Sentenceizer(rawText: String) {
-  val sentences = List(rawText)
+  private val sentences: Array[String] = rawText.split("\n")
+
+  private[this] def validate_sentences(): Array[String] = {
+  	// TODO 
+  	sentences
+  }
+
+  validate_sentences()
+
+  def get_list_of_sentences(): List[String] = sentences.toList
+
+  def get_sentence_by_number(sentence_no: Int) = sentences(sentence_no - 1)
 }
--- src/main/scala/Tokenizer.scala
+++ src/main/scala/Tokenizer.scala
@@ -1,5 +1,18 @@
 package nltk
 
 class Tokenizer(sents: Sentenceizer) {
+  
+  private[this] def get_tokens_of_sentence(sentence: String): List[String] = {
+  	val words: List[String] = sentence.split("\\s+").map(_.trim).toList
+  	// TODO split actual words from punctuation marks
+  	words
+  }
+
+  private val sentences = sents.get_list_of_sentences()
+  private val tokens_by_sequence: List[List[String]] = sentences.map(get_tokens_of_sentence(_))
+
+  // val tokens = tokens_by_sequence.flatten
+
   val tokens = List("I","compute","therefore","I","am")
+
 }
